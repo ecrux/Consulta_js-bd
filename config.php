ba0b7b0be609729458871bea71c7b2ae2d6f90d4
Edwar Cruz <estebancruz373@gmail.com>
2017-04-17 09:38:40 -0500

    Código de prueba
    Este es mi código limpio y de prueba para consulta a angular en base de datos.

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+   
+    $servidor = "localhost";
+    $usuario = "root";
+    $clave = "";
+    $base_de_datos = "mi_bd_angular";
+    
+?>
---
+++ llamado-php.php
@@ -0,0 +1,45 @@
+<?php
+
+    /**
+     * Este php me permite usar el poder del php con un ejemplo de la tecnología AngularJS...
+     * incluso desde el administrador de este sitio.
+     */
+
+    if( isset( $_GET[ 'cadena' ] ) )
+    {     
+        include( "config.php" );
+        
+        /*Esta conexión se realiza para la prueba con angularjs*/
+        header("Access-Control-Allow-Origin: *");
+        header("Content-Type: application/json; charset=UTF-8");
+        
+        /*$conn = new mysqli( $servidor, $usuario, $clave, $base_de_datos );
+        
+        //Se busca principalmente por alias.
+        $sql  = " SELECT * FROM tb_usuarios  ";
+        $sql .= " WHERE alias LIKE '%".$_GET[ 'cadena' ]."%'  ";
+        $sql .= " ORDER BY fecha_registro DESC LIMIT 10 ";
+        
+        //LA tabla que se cree debe tener la tabla aquí requerida, y los campos requeridos abajo.
+        $result = $conn->query( $sql );
+        
+        $outp = "";
+        
+        while($rs = $result->fetch_array( MYSQLI_ASSOC )) 
+        {
+            //Mucho cuidado con esta sintaxis, hay una gran probabilidad de fallo con cualquier elemento que falte.
+            if ($outp != "") {$outp .= ",";}
+            $outp .= '{"Alias":"'.$rs["alias"].'",';            // <-- La tabla MySQL debe tener este campo.
+            $outp .= '"Nombres":"'.$rs["nombres"].'",';         // <-- La tabla MySQL debe tener este campo.
+            $outp .= '"Documento":"'.$rs["documento"].'",'; 
+            $outp .= '"Apellidos":"'.$rs["apellidos"].'"}';     // <-- La tabla MySQL debe tener este campo.
+        }
+        
+        $outp ='{"records":['.$outp.']}';*/
+        $salida = '{"records":[{"Alias" : "TITO"}]}';
+        //$conn->close();
+        echo $salida;
+        //echo($outp);
+    
+    }
+?> 
